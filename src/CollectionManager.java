42c31e75a2fb6285fdf50e5660e9667c1b09cd7a
Nerr4life <84106087+Nerr4life@users.noreply.github.com>
2021-09-19 22:06:46 -0400

    Update CollectionManager.java

--- src/CollectionManager.java
+++ src/CollectionManager.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
 
 public class CollectionManager {
 	
 
 	public void run() {
-		
+		System.out.println("Collection Manager starts running.");
+		Scanner console = new Scanner(System.in);
+		String str = sc.nextLine();
+
+		StringTokenizer tokenizer = new StringTokenizer(str);
+		while (!(tokenizer.nextElement().equals("Q"))){
+
+		}
+		System.out.println("Collection Manager terminated.");
 	}
 }
