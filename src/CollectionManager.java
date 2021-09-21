f572100c47e97b63c49a3b03258b2adce8a9adb2
Jeevan Vasanthan <jeevan.vasanthan@gmail.com>
2021-09-21 19:04:24 -0400

    Modified CollectionManager class

--- src/CollectionManager.java
+++ src/CollectionManager.java
@@ -1,17 +1,48 @@
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class CollectionManager {
 	
-
 	public void run() {
 		System.out.println("Collection Manager starts running.");
-		Scanner console = new Scanner(System.in);
+		boolean isRunning=true;
+		Collection collection=new Collection();
+		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
-
-		StringTokenizer tokenizer = new StringTokenizer(str);
-		while (!(tokenizer.nextElement().equals("Q"))){
-
+		StringTokenizer st = new StringTokenizer(str,",");
+		while(isRunning) {
+			if(st.nextToken()=="A") {
+				Album a=new Album(st.nextToken(),st.nextToken(),(Genre)st.nextElement(),(Date)st.nextElement(),true);
+				if(collection.add(a))
+					System.out.println(a.toString());//not complete
+				else
+					System.out.println(a.toString());//not complete
+			}
+			else if(st.nextToken()=="D") {
+				Album a=new Album(st.nextToken(), st.nextToken(), null, null,true);
+				if(collection.remove(a))
+					System.out.println(a.toString()); //not complete
+				else
+					System.out.println(a.toString()); //not complete
+			}
+			else if(st.nextToken()=="L") {
+				
+			}
+			else if(st.nextToken()=="R") {
+				
+			}
+			else if(st.nextToken()=="Q") {
+				System.out.println("Collection Manager terminated.");
+				sc.close();
+				isRunning=false;
+			}
+			else {
+				System.out.println("Collection Manager terminated.");
+			}
+			if(isRunning) {
+				str = sc.nextLine();				//reads next line of input
+				st = new StringTokenizer(str,",");	//resets string tokenizer
+			}
 		}
-		System.out.println("Collection Manager terminated.");
 	}
 }
